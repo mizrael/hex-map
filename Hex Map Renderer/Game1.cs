49f8fbd1e052e92ab9b0c025726f3dcb36b3a0e1
mizrael <mizrael@yahoo.it>
2013-09-16 00:41:27 +0200

    first commit

---
+++ Hex Map Renderer/Game1.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace HexMapRenderer
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        
+        HexMap _hexMap;
+
+        Vector2 _cameraPos = Vector2.Zero;
+        float _cameraZoom = 1f;
+        Matrix _cameraMatrix;
+
+        Vector3 _halfScreenSize;
+
+        KeyboardState _lastState;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            this.IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var hexTexture = this.Content.Load<Texture2D>("tiles");
+            var tileSize = new Vector2(hexTexture.Width / 5, hexTexture.Height / 2);            
+            
+            _hexMap = new HexMap(5, 5, tileSize, hexTexture);
+
+            _halfScreenSize = new Vector3(GraphicsDevice.Viewport.Width * 0.5f, GraphicsDevice.Viewport.Height * 0.5f, 0);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            var mouseState = Mouse.GetState();
+            _hexMap.SelectTile(new Vector2(mouseState.X, mouseState.Y));
+
+
+            var cameraOffset = 5f;
+
+            var keyboardState = Keyboard.GetState();
+
+            if (_lastState.IsKeyDown(Keys.W) && keyboardState.IsKeyDown(Keys.W))
+                _cameraPos.Y -= cameraOffset;
+            else if (_lastState.IsKeyDown(Keys.S) && keyboardState.IsKeyDown(Keys.S))
+                _cameraPos.Y += cameraOffset;
+
+            if (_lastState.IsKeyDown(Keys.A) && keyboardState.IsKeyDown(Keys.A))
+                _cameraPos.X -= cameraOffset;
+            else if (_lastState.IsKeyDown(Keys.D) && keyboardState.IsKeyDown(Keys.D))
+                _cameraPos.X += cameraOffset;
+
+            if (_lastState.IsKeyDown(Keys.Q) && keyboardState.IsKeyDown(Keys.Q))
+                _cameraZoom += 0.01f;
+            else if (_lastState.IsKeyDown(Keys.E) && keyboardState.IsKeyDown(Keys.E))
+                _cameraZoom -= 0.01f;
+
+            _lastState = keyboardState;
+
+            _cameraMatrix = Matrix.CreateTranslation(new Vector3(-_cameraPos.X, -_cameraPos.Y, 0)) *                                                    
+                                                     Matrix.CreateScale(new Vector3(_cameraZoom, _cameraZoom, 1)) *
+                                                     Matrix.CreateTranslation(_halfScreenSize);            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            //spriteBatch.Begin(SpriteSortMode.BackToFront, null, null, null, null, null, _cameraMatrix);            
+            spriteBatch.Begin();
+
+            _hexMap.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+
+}
---
+++ Hex Map Renderer/HexMap.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HexMapRenderer
+{
+
+    public class HexTile
+    {
+        public Rectangle SourceRectangle;
+        public Vector2 Position;
+        public bool Selected;
+    }
+
+    public class HexMap
+    {
+        #region Members
+
+        private HexTile[,] _tiles;
+        private Vector2 _tileSize;
+        private Vector2 _hexOffset;
+        private Texture2D _tilesAsset;
+
+        private HexTile _selectedTile;
+
+        #endregion Members
+
+        public HexMap(int tileCountX, int tileCountY, Vector2 tileSize, Texture2D tilesAsset)
+        {
+            _tileSize = tileSize;
+            _hexOffset = new Vector2(_tileSize.X * 0.75f, _tileSize.Y * -0.5f);
+
+            _tiles = new HexTile[tileCountX, tileCountY];
+            _tilesAsset = tilesAsset;
+
+            SetupTiles();
+        }
+
+        #region Methods
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var mapCullingBounds = ComputeMapCulling();
+
+            for (int y = mapCullingBounds.Y; y != mapCullingBounds.Height; ++y)
+            {
+                for (int x = mapCullingBounds.X; x != mapCullingBounds.Width; ++x)
+                {
+                    var currTile = _tiles[x, y];
+                    spriteBatch.Draw(_tilesAsset, currTile.Position, currTile.SourceRectangle, 
+                                     currTile.Selected ? Color.Red : Color.White, 
+                                     0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+                }
+            }
+        }
+
+        /// <summary>
+        /// WIP
+        /// http://gamedev.stackexchange.com/questions/20742/how-can-i-implement-hexagonal-tilemap-picking-in-xna
+        /// </summary>
+        /// <param name="mousePos"></param>
+        public void SelectTile(Vector2 mousePos) 
+        {
+            if (null != _selectedTile)
+            {
+                _selectedTile.Selected = false;
+                _selectedTile = null;
+            }
+
+            var x = mousePos.X;
+            var y = mousePos.Y;
+
+            var h = _tileSize.Y;
+            var W = _tileSize.X;
+            var w = _tileSize.X * 0.5f;
+
+            var i = (int)((x * 2f) / (W + w));
+            var j = (int)((y * 2f) / h);
+
+            if (i < 0 || j < 0)
+                return;
+                        
+            var u = x - (i * 0.5f * (W + w));
+            var v = y - (j * h * 0.5f);
+
+            var isGreenArea = (u < (W - w) * 0.5f);
+            if (isGreenArea) {
+                var isUpper = (IsEven(ref i) && IsEven(ref j));
+
+                u = 2 * u / (W - w);
+                v = 2 * v / h;
+
+                if ((isUpper && ((1 - v) > u) || (!isUpper && v < u)))
+                    i--;
+
+                if (IsEven(ref i))
+                    j--;
+
+                j = (int)Math.Floor((double)j * 0.5);
+            }
+
+            ClampIndices(ref i, ref j);
+
+            _selectedTile = _tiles[i, j];
+
+            _selectedTile.Selected = true;
+        }
+
+        private void ClampIndices(ref int i, ref int j) {
+            if (i >= _tiles.GetLength(0))
+                i = _tiles.GetLength(0) - 1;
+            if (i < 0) i = 0;
+            if (j >= _tiles.GetLength(1))
+                j = _tiles.GetLength(1) - 1;
+            if (j < 0) j = 0;
+        }
+
+        private static bool IsEven(ref int value) {
+            return 0 == (value & 1);
+        }
+
+        #endregion Methods
+
+        #region Private Methods
+
+        private void SetupTiles()
+        {
+            var hexPos = Vector2.Zero;
+            
+            var sourceRect = new Rectangle((int)_tileSize.X * 2, 0, (int)_tileSize.X, (int)_tileSize.Y);            
+
+            for (int y = 0; y != _tiles.GetLength(1); ++y)
+            {
+                // ISO
+                //hexPos.Y = _tileSize.Y * y * .5f;
+                //hexPos.X = _hexOffset.X * y;     
+
+                // GRID
+                hexPos.Y = _tileSize.Y * y - _hexOffset.Y;
+                hexPos.X = -_hexOffset.X;
+
+                for (int x = 0; x != _tiles.GetLength(0); ++x)
+                {
+                    // ISO
+                    //hexPos += _hexOffset;            
+        
+                    // GRID
+                    hexPos.X += _hexOffset.X;
+                    hexPos.Y += ((x & 1) == 1) ? -_hexOffset.Y : _hexOffset.Y;
+
+                    _tiles[x, y] = new HexTile()
+                    {
+                        Selected = false,
+                        SourceRectangle = sourceRect,
+                        Position = hexPos
+                    };
+                }
+            }
+        }
+
+        private Rectangle ComputeMapCulling()
+        {
+            /*var bounding = Rectangle.Empty;
+
+            //midTile = GetCentreTile(gameCamera);
+            var midTile = new Vector2(0, 0);
+            bounding.Width = (int)(midTile.X + Math.Round((_halfScreenSize.X / (_tileSize.X)) / _cameraZoom));
+            bounding.X = (int)(midTile.X - Math.Round((_halfScreenSize.X / (_tileSize.X)) / _cameraZoom));
+            bounding.Height = (int)(midTile.Y + Math.Round((_halfScreenSize.Y / (_tileSize.Y)) / _cameraZoom));
+            bounding.Y = (int)(midTile.Y - Math.Round((_halfScreenSize.Y / (_tileSize.Y)) / _cameraZoom));
+
+            bounding.X = bounding.X >= 0 ? bounding.X : 0;
+            bounding.Width = bounding.Width <= tileCountX - 1 ? bounding.Width : tileCountX - 1;
+            bounding.Y = bounding.Y >= 0 ? bounding.Y : 0;
+            bounding.Height = bounding.Height <= tileCountY - 1 ? bounding.Height : tileCountY - 1;
+
+            return bounding;*/
+
+            return new Rectangle(0, 0, _tiles.GetLength(0), _tiles.GetLength(1));
+        }
+
+        #endregion Private Methods
+    }
+}
