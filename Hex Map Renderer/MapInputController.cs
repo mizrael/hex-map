93e0ba7148a4936aa8b4824813e8d7f56b1e26aa
mizrael <davide.guida@razorfishhealthware.com>
2013-10-04 12:40:02 +0200

    updated draw method to use camera zoom

--- Hex Map Renderer/MapInputController.cs
+++ Hex Map Renderer/MapInputController.cs
@@ -26,16 +26,91 @@ namespace HexMapRenderer
 
         private Path<HexTile> _foundPath;
 
+        private Func<HexTile, HexTile, double> _distanceFunc;
+        private Func<HexTile, HexTile, double> _estimateFunc;
+        private Func<HexTile, IEnumerable<HexTile>> _findNeighboursFunc;
+
+        private CameraService _camera;
+
         #endregion Members
 
-        public MapInputController(Game game) : base(game) {
-            base.Visible = this.Enabled = false;              
+        public MapInputController(Game game)
+            : base(game)
+        {
+            base.Visible = this.Enabled = false;
+
+            _distanceFunc = (t1, t2) => 0;
+            _estimateFunc = (t1, t2) => Vector2.DistanceSquared(t1.Position, t2.Position);
+            _findNeighboursFunc = t => { return _map.GetNeighbours(t).Where(nt => nt.TileType == HexTile.TileTypes.Walkable); };
         }
+
+        #region DrawableGameComponent overrides
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(base.GraphicsDevice);
+            base.LoadContent();
+
+            _spriteBatch = new SpriteBatch(base.GraphicsDevice);            
         }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _camera = base.Game.Services.GetService(typeof(CameraService)) as CameraService;
+            if (null == _camera)
+                throw new Exception("Unable to find CameraService");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var state = Keyboard.GetState();
+
+            if (state.IsKeyUp(Keys.Enter) && _lastKeyState.IsKeyDown(Keys.Enter) && 2 == _endpoints.Count)
+                _foundPath = Pathfinder.FindPath<HexTile>(_endpoints.ElementAt(0), _endpoints.ElementAt(1),
+                                                          _distanceFunc, _estimateFunc, _findNeighboursFunc);
+
+            _lastKeyState = state;
+
+            UpdateMouse();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.Additive);
+
+            Vector2 tmpPos;
+            var cursorSize = _cursorRadius * _camera.Zoom;
+
+            if (null != _foundPath)
+            {
+                foreach (var node in _foundPath)
+                {
+                    ComputCursorPosition(node, out tmpPos);
+                    _spriteBatch.DrawCircle(tmpPos, cursorSize * 0.5f, 6, Color.PowderBlue, 3f);
+                }
+            }
+
+            if (0 != _endpoints.Count) {
+                foreach (var point in _endpoints)
+                {
+                    ComputCursorPosition(point, out tmpPos);
+                    _spriteBatch.DrawCircle(tmpPos, cursorSize, 6, Color.Red, 3f);
+                }
+            }
+
+            if (null != _hoverTile)
+            {
+                ComputCursorPosition(_hoverTile, out tmpPos);
+                _spriteBatch.DrawCircle(tmpPos, cursorSize, 6, Color.Green, 3f);
+            }
+
+            _spriteBatch.End();
+        }
+
+        #endregion DrawableGameComponent overrides
+
+        #region Public Methods
 
         public void SetMap(HexMap map)
         {
@@ -48,54 +123,21 @@ namespace HexMapRenderer
             _cursorRadius = _halfTileSize.Y;
 
             _hoverTile = null;
-            
+
             base.Visible = base.Enabled = (null != _map);
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            var state = Keyboard.GetState();
-
-            if (state.IsKeyUp(Keys.Enter) && _lastKeyState.IsKeyDown(Keys.Enter) && 2 == _endpoints.Count)
-            {
-                Func<HexTile, HexTile, double> distance = (t1, t2) => { return (t1.TileType == HexTile.TileTypes.Wall || t2.TileType == HexTile.TileTypes.Wall) ? 1000 : 0; };
-                Func<HexTile, HexTile, double> estimate = (t1, t2) => Vector2.DistanceSquared(t1.Position, t2.Position);
-                Func<HexTile, IEnumerable<HexTile>> findNeighbours = t =>{return _map.GetNeighbours(t).Where(nt => nt.TileType == HexTile.TileTypes.Walkable);};
-                
-                _foundPath = Pathfinder.FindPath<HexTile>(_endpoints.ElementAt(0), _endpoints.ElementAt(1),
-                                                          distance, estimate, findNeighbours);
-            }
-
-            _lastKeyState = state;
-
-            UpdateMouse();
-        }
-
-        public override void Draw(GameTime gameTime)
-        {
-            _spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.Additive);
-
-            if (null != _foundPath)
-            {
-                foreach (var node in _foundPath)
-                {
-                    _spriteBatch.DrawCircle(node.Position + _halfTileSize, _cursorRadius * 0.5f, 6, Color.PowderBlue, 3f);
-                }
-            }
-
-            if (0 != _endpoints.Count) { 
-                foreach(var point in _endpoints)
-                    _spriteBatch.DrawCircle(point.Position + _halfTileSize, _cursorRadius, 6, Color.Red, 3f);
-            }
-
-            if (null != _hoverTile)
-                _spriteBatch.DrawCircle(_hoverTile.Position + _halfTileSize, _cursorRadius, 6, Color.Green, 3f);           
-
-            _spriteBatch.End();
         }
 
         public IEnumerable<HexTile> GetEndpoints() {
             return _endpoints.ToArray();
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private void ComputCursorPosition(HexTile tile, out Vector2 position) { 
+            position = tile.Position + _halfTileSize;
+            Vector2.Transform(ref position, ref _camera.Matrix, out position);
         }
 
         private void UpdateMouse()
@@ -123,6 +165,7 @@ namespace HexMapRenderer
             _lastMouseState = mouseState;
         }
 
+        #endregion Private Methods
     }    
 }
 
